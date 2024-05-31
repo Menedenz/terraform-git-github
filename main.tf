7ea266cbfbb75b9b3279748613de09ed6d192a92
Bashar <abmh20012000@gmail.com>
2024-05-31 17:58:56 +0300

    version 2

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "EC2" {
     user_data_replace_on_change = true
 
     tags = {
-        name = "test_git-EC2-vers-git-1"
+        name = "test_git-EC2-vers-git-2"
     }
 }
 
